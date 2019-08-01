fed25ce563dc659db7435c2c3ba966b26e557961
Smart Tool Factory <smarttoolfactory@icloud.com>
2019-08-01 23:19:50 +0300

    Bug fix, database implementation is commented out for now

--- app/src/main/java/com/smarttoolfactory/movieapp/data/source/local/MoviesDao.kt
+++ app/src/main/java/com/smarttoolfactory/movieapp/data/source/local/MoviesDao.kt
@@ -1,21 +1,21 @@
-package com.smarttoolfactory.movieapp.data.source.local
-
-import androidx.room.Dao
-import com.smarttoolfactory.movieapp.data.model.Movie
-import com.smarttoolfactory.movieapp.data.model.Movies
-import io.reactivex.Flowable
-import io.reactivex.Observable
-
-/**
- * Data Access Object for the tasks table.
- */
-@Dao
-interface MoviesDao : BaseDao<Movie> {
-
-    fun getMovies(page: Int): Flowable<List<Movie>>
-
-    fun getMoviesSortedBy( page: Int, sortBy: String): Flowable<List<Movie>>
-
-    fun getMovies(queryMap : Map<String, String>): Flowable<List<Movie>>
-
-}
+//package com.smarttoolfactory.movieapp.data.source.local
+//
+//import androidx.room.Dao
+//import com.smarttoolfactory.movieapp.data.model.Movie
+//import com.smarttoolfactory.movieapp.data.model.Movies
+//import io.reactivex.Flowable
+//import io.reactivex.Observable
+//
+///**
+// * Data Access Object for the tasks table.
+// */
+//@Dao
+//interface MoviesDao : BaseDao<Movie> {
+//
+//    fun getMovies(page: Int): Flowable<List<Movie>>
+//
+//    fun getMoviesSortedBy( page: Int, sortBy: String): Flowable<List<Movie>>
+//
+//    fun getMovies(queryMap : Map<String, String>): Flowable<List<Movie>>
+//
+//}
